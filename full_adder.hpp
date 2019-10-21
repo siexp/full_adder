fdbb748160d63d1d8d68e9da0a2e163619dfb0fb
Mark Ts <elioni17@gmail.com>
2019-10-22 00:15:10 +0300

    added overflow check

--- full_adder.hpp
+++ full_adder.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <stdexcept>
+#include <limits>
+
 int add(int rhs, int lhs)
 {
+    constexpr int negative_bit_mask = 1 << std::numeric_limits<int>::digits;
+
+    const bool sum_is_negative = (rhs & negative_bit_mask) && (lhs & negative_bit_mask);
+    const bool sum_is_positive = !(rhs & negative_bit_mask) && !(lhs & negative_bit_mask);
+
     while (lhs != 0)
     {
         // holds which bits are overflowed
@@ -9,8 +17,15 @@ int add(int rhs, int lhs)
 
         // half-sum
         rhs = rhs ^ lhs;
-        
+
         lhs = carry << 1;
+    }
+
+    const bool negative_oveflow = sum_is_negative && !(rhs & negative_bit_mask);
+    const bool positive_oveflow = sum_is_positive && (rhs & negative_bit_mask);
+    if( negative_oveflow || positive_oveflow )
+    {
+        throw std::runtime_error{"overflow"};
     }
 
     return rhs;
---
+++ test.cpp
@@ -0,0 +1,86 @@
+#include "full_adder.hpp"
+
+#include <stdexcept>
+#include <iostream>
+#include <limits>
+
+void positive_plus_positive_no_overflow_test()
+{
+    if (add(5, 10) != 15)
+    {
+        throw std::logic_error{"5 + 10 != 15"};
+    }
+}
+
+void positive_plus_negative_no_overflow_test()
+{
+    if (add(5, -10) != -5)
+    {
+        throw std::logic_error{"5 + (-10) != -5"};
+    }
+}
+
+void negative_plus_positive_no_overflow_test()
+{
+    if (add(-5, 10) != 5)
+    {
+        throw std::logic_error{"(-5) + 10 != 5"};
+    }
+}
+
+void negative_plus_negative_no_overflow_test()
+{
+    if (add(-5, -10) != -15)
+    {
+        throw std::logic_error{"(-5) + (-10) != -15"};
+    }
+}
+
+
+void positive_plus_positive_overflow_test()
+{
+    try
+    {
+        add(5, std::numeric_limits< int >::max() );
+        throw std::logic_error{"no overflow 5 + std::numeric_limits< int >::max()"};
+    }
+    catch(const std::runtime_error&)
+    {
+        // expected exception
+    }
+}
+
+
+void negative_plus_negative_overflow_test()
+{
+    try
+    {
+        add(-5, std::numeric_limits< int >::min() );
+        throw std::logic_error{"no overflow (-5) + std::numeric_limits< int >::min()"};
+    }
+    catch(const std::runtime_error&)
+    {
+        // expected exception
+    }
+}
+
+int main()
+{
+    try
+    {
+        positive_plus_positive_no_overflow_test();
+        positive_plus_negative_no_overflow_test();
+        negative_plus_positive_no_overflow_test();
+        negative_plus_negative_no_overflow_test();
+
+        positive_plus_positive_overflow_test();
+        negative_plus_negative_overflow_test();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
